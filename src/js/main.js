0ecab9f677a19fe820f499e9cb5475252836ab7b
Rick Mohr <rmohr@azavea.com>
2013-01-29 10:45:23 -0500

    Jumpstart network site HTML / CSS / JavaScript
    JSON: since the site hierarchy is still under discussion and may well not match the structure in the spec's example sites.json file I used a simplified flat structure for sites.json. We'll fix it when the structure is decided.
    
    CSS:
    I put basic styles into site.css to keep the HTML5 boilerplate in main.css clean and separate.
    All of Mike's designs have similar structure for the site blocks so I added basic styles to block out the map thumb / title / description / link.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
+function loadSiteData() {
+    $.ajax({
+        url: 'sites.json',
+        dataType: 'json',
+        cache: false,
+        success: showSiteData,
+        error: function(resp) {
+            alert('Error getting site data');
+        }
+    });
+}
+
+function showSiteData(siteData) {
+    var template = $('#template-site-blocks').html();
+    var output = _.template(template, siteData);
+    $('#site-blocks').html(output);
+}
 
