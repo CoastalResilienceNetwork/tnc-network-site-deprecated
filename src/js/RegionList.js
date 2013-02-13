234e2063039342dcbd1f7bb16c982c4de7fffd6f
Matthew McFarland <mmcfarland@azavea.com>
2013-02-13 15:55:57 -0500

    Render hierarchical region list
    Load the sites.json from the server and render the list or regions and nested
    sub regions.

---
+++ src/js/RegionList.js
@@ -0,0 +1,59 @@
+(function (N) {
+    N.RegionList = function regionList() {
+
+        function renderListAndMap() {
+            $.ajax({
+                url: 'sites.json',
+                dataType: 'json',
+                cache: false,
+                success: showSiteData,
+                error: function(resp) {
+                    alert('Error getting site data');
+                }
+            });
+        }
+
+        function showSiteData(sites) {
+            var regionsMarkup = _.map(sites.regions, renderRegion),
+                $sitesList = $('#site-blocks')
+
+            // Add global region to the top
+            regionsMarkup.unshift(renderRegion(sites.global));
+
+            $sitesList.append.apply($sitesList, regionsMarkup);
+
+            // Collapse all regions, and toggle visibility on click
+            $sitesList.find('.region-header')
+                .on('click', function(evt) {
+                    $(evt.target).siblings().slideToggle();
+                })
+                .siblings().hide();
+        }
+
+        function renderRegion(region) {
+            // Render a region template
+            var $markup =  $(N.app.templates['template-region'](region));
+
+            // Recursively render any sub-regions
+            if (region.regions) {
+                var subregions = _.map(region.regions, function renderSubRegions(subregion) {
+                    return renderRegion(subregion);
+                });
+
+                var $sublist = $markup.find('.region-detail ul');
+                $sublist.append.apply($sublist, subregions);
+            }
+
+            // A complete nested region
+            return $markup;
+        }
+
+        return {
+            renderListAndMap: renderListAndMap
+        }
+    }
+
+}(TNC));
+
+
+
