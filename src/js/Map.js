7ef42b940efc91306648615eb159525e33531d24
Matthew McFarland <mmcfarland@azavea.com>
2013-02-14 12:00:56 -0500

    Add map functions to project
    Use ESRI JSAPI to add markers to the map, using it's special cumbersome methods.
    
    Clicking a marker will expand and scroll to the region list.

---
+++ src/js/Map.js
@@ -0,0 +1,72 @@
+(function(N) {
+    N.Map = function networkMap() {
+        var _map;
+
+        function initializeMap(callback) {
+            _map = new esri.Map('map', {
+                basemap: 'gray',
+                center: [-50.4, 42.0],
+                zoom: 3,
+                sliderStyle: 'small'
+            });
+
+            dojo.connect(_map, 'onLoad', function() {
+                dojo.connect(_map.graphics,'onClick', scrollToRegion);
+                dojo.connect(_map.graphics, 'onMouseOver', markerMouseOver);
+                dojo.connect(_map.graphics, 'onMouseOut', markerMouseOut);
+
+                // Proceed only when the map has loaded, when layers are ready
+                callback();
+            });
+
+            return this;
+        }
+
+        function markerMouseOver(evt) {
+            _map.setMapCursor('pointer');
+            if (evt.graphic) {
+                $(evt.target).tipsy({
+                    gravity: 's',
+                    title: function() {
+                        return evt.graphic.attributes.name
+                    }
+                });
+            }
+        }
+
+        function markerMouseOut(evt) {
+            _map.setMapCursor('default');
+        }
+
+        function scrollToRegion(evt) {
+            if (evt.graphic) {
+                var $el = evt.graphic.attributes.$el;
+                $el.parent().find('.region-header').siblings().slideUp();
+                $el.children().slideDown();
+                $.scrollTo($el, 300);
+            }
+        }
+
+        function addMarker(region, el) {
+            var marker = new esri.symbol.PictureMarkerSymbol({
+                    "url":"img/map_marker.png",
+                    "height":40,
+                    "width":50,
+                    "type":"esriPMS"
+                }),
+                point = new esri.geometry.geographicToWebMercator(
+                  new esri.geometry.Point(region.location[0],region.location[1])
+                ),
+                attributes = {name: region.name, $el: $(el)},
+                graphic = new esri.Graphic(point, marker, attributes);
+
+            _map.graphics.add(graphic);
+        }
+
+        return {
+            init: initializeMap,
+            addMarker: addMarker,
+            esriMap: _map
+        }
+    }
+}(TNC));
