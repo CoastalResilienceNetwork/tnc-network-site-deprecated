34207b6a6b5b9d35b0e8f185c15326a5d3c8e417
Kevin DeLoach <kdeloach@azavea.com>
2014-04-07 13:17:38 -0400

    Issue #809: Allow selected tooltips to be permanently displayed.
    Added a new region option called "stickyTooltip" in sites.json
    which will keep the marker tooltip permanently visible when true.
    
    This change also upgrades the esri API from 3.3 to 3.8. I needed to
    upgrade to use a method that was recently added called getNode() which
    gives access to the esri.Graphic DOM element.
    
    I also updated the tipsy jQuery plugin to the latest version and fixed a
    bug which prevented the tooltip tails from displaying.

--- src/js/Map.js
+++ src/js/Map.js
@@ -1,10 +1,11 @@
 (function(N) {
     N.Map = function networkMap() {
         var _map,
+            _markers = [],
             pointSymbol = new esri.symbol.SimpleMarkerSymbol(
                 esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 2,
                 new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-                    new dojo.Color([105, 105, 105]), 1),
+                new dojo.Color([105, 105, 105]), 1),
                 new dojo.Color([80, 80, 80, 0.35]));
 
         function initializeMap(callback) {
@@ -17,6 +18,10 @@
             });
 
             dojo.connect(_map, 'onLoad', function() {
+                dojo.connect(_map, 'onPan', updateTooltips);
+                dojo.connect(_map, 'onZoomStart', hideTooltips);
+                dojo.connect(_map, 'onZoomEnd', showTooltips);
+                dojo.connect(_map, 'onExtentChange', showTooltips);
                 dojo.connect(_map.graphics,'onClick', scrollToRegion);
                 dojo.connect(_map.graphics, 'onMouseOver', markerMouseOver);
                 dojo.connect(_map.graphics, 'onMouseOut', markerMouseOut);
@@ -31,20 +36,57 @@
         function markerMouseOver(evt) {
             _map.setMapCursor('pointer');
             if (evt.graphic && evt.graphic.attributes) {
-                var el = N.SvgDomWrapper(evt.target);
-                $(el).tipsy({
-                    gravity: 's',
-                    title: function() {
-                        return evt.graphic.attributes.name
-                    },
-                    trigger: 'manual'
-                }).tipsy('show');
+                displayTooltip(evt.target, evt.graphic.attributes.name);
             }
         }
 
         function markerMouseOut(evt) {
             _map.setMapCursor('default');
-            $(evt.target).tipsy('hide');
+            if (evt.graphic && evt.graphic.attributes) {
+                if (!evt.graphic.attributes.isSticky) {
+                    hideTooltip(evt.target);
+                }
+            }
+        }
+
+        function displayTooltip(el, tooltipText) {
+            if (!el) {
+                return;
+            }
+            el = N.SvgDomWrapper(el);
+            $(el).tipsy({
+                gravity: 's',
+                title: function() {
+                    return tooltipText;
+                },
+                trigger: 'manual'
+            }).tipsy('show');
+        }
+
+        function hideTooltip(el) {
+            $(el).tipsy('hide');
+        }
+
+        function hideTooltips(evt) {
+            $.fn.tipsy.revalidate();
+            $.each(_markers, function(i, graphic) {
+                hideTooltip(graphic.getNode());
+            });
+        }
+
+        // Tooltips end up in weird places if we call updateTooltips immediately
+        // when handling the map onZoomEnd and onExtentChange events.
+        function showTooltips(evt) {
+            _.defer(updateTooltips);
+        }
+
+        function updateTooltips(evt) {
+            $.fn.tipsy.revalidate();
+            $.each(_markers, function(i, graphic) {
+                if (graphic.attributes.isSticky) {
+                    displayTooltip(graphic.getNode(), graphic.attributes.name);
+                }
+            });
         }
 
         function scrollToRegion(evt) {
@@ -66,12 +108,20 @@
                 point = new esri.geometry.geographicToWebMercator(
                   new esri.geometry.Point(region.location[0],region.location[1])
                 ),
-                attributes = {name: region.name, $el: $(el)},
+                tooltipText = region.name,
+                isSticky = !!region.stickyTooltip,
+                attributes = {name: tooltipText, $el: $(el), isSticky: isSticky},
                 graphic = new esri.Graphic(point, marker, attributes),
                 smallPoint = new esri.Graphic(point, pointSymbol);
 
+            _markers.push(graphic);
+
             _map.graphics.add(graphic);
             _map.graphics.add(smallPoint);
+
+            if (isSticky) {
+                displayTooltip(graphic.getNode(), tooltipText);
+            }
         }
 
         return {
